dd5232a050640c0ac0b54d28b3f79e2b5cd37bf1
Mirko Kiefer <mail@mirkokiefer.com>
2014-02-17 00:07:50 +0100

    convenience script to remove previous container on restart

--- example/node-app/run.sh
+++ example/node-app/run.sh
@@ -1,3 +1,7 @@
 
+OLD=$(docker kill $(docker ps | grep "mirkokiefer/example1:latest" | awk {'print $1'}))
+echo "remove $OLD"
+docker rm $OLD
+
 bash build.sh
 docker run -d -p 8080:8080 -v /var/example1:/logs:rw mirkokiefer/example1
